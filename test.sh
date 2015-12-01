4e5fdd4af5596969569fa96a506430021ce6dd55
Boris Bügling <boris@icculus.org>
2015-12-01 13:36:05 +0100

    Rename generator script

--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@
 . /usr/local/opt/chswift/share/chswift/chswift.sh
 chswift 2.1
 
-FRAMEWORKS=$HOME/.📦/ctf-generator/Rome
+FRAMEWORKS=$HOME/.📦/contentful-generator/Rome
 
-./bin/ctf-generator cfexampleapi b4c0n73n7fu1
+./bin/contentful-generator cfexampleapi b4c0n73n7fu1
 xcrun --sdk macosx swiftc -F$FRAMEWORKS -framework Contentful \
   -module-name YOLO -c *.swift
 rm -f *.o
