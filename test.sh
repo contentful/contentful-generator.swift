53cd38b28b7504d5b9958c0aece4d59e9b6290f8
Boris Bügling <boris@icculus.org>
2015-12-01 14:35:46 +0100

    Add option for customizing the output directory

--- test.sh
+++ test.sh
@@ -5,7 +5,11 @@ chswift 2.1
 
 FRAMEWORKS=$HOME/.📦/contentful-generator/Rome
 
-./bin/contentful-generator cfexampleapi b4c0n73n7fu1
+mkdir -p out
+
+./bin/contentful-generator cfexampleapi b4c0n73n7fu1 --output out
+
+cd out
 xcrun --sdk macosx swiftc -F$FRAMEWORKS -framework Contentful \
   -module-name YOLO -c *.swift
 rm -f *.o
