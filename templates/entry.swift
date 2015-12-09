bfa8d598f64d7c6f853540ff14c7d69201b5dd3e
Boris Bügling <boris@icculus.org>
2015-12-09 19:39:51 +0100

    Generate a `fromEntry()` function for conversion

--- templates/entry.swift
+++ templates/entry.swift
@@ -2,8 +2,14 @@
 
 import Contentful
 import CoreLocation
+import Decodable
 
 struct {{ className }} {{% for field in fields %}
   let {{ field.name }}: {{ field.type }}{% endfor %}
+
+  static func fromEntry(entry: Entry) throws -> {{ className }} {
+    return {{ className }}({% for field in fields %}
+        {{ field.name }}: try (entry.fields as! AnyObject) => "{{ field.name }}",{% endfor %})
+  }
 }
 
