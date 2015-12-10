6812d4e5d47da9d6eac71dc2e2b94e64edab4795
Boris Bügling <boris@icculus.org>
2015-12-10 08:13:45 +0100

    Put Contentful SDK specific code in an extension

--- templates/entry.swift
+++ templates/entry.swift
@@ -1,14 +1,16 @@
 // This is a generated file.
 
-import Contentful
 import CoreLocation
 
 struct {{ className }} {{% for field in fields %}
   let {{ field.name }}: {{ field.type }}?{% endfor %}
+}
 
+import Contentful
+
+extension {{ className }} {
   static func fromEntry(entry: Entry) throws -> {{ className }} {
     return {{ className }}({% for field in fields %}
         {{ field.name }}: entry.fields["{{ field.name }}"] as? {{ field.type }},{% endfor %})
   }
 }
-
